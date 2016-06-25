45d1fe639e58cce948b78fc5e991cc267684934a
Leeroy <zgadiane@gmail.com>
2016-06-25 19:12:55 +0800

    second commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        
+        <link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap-theme.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap/dist/css/site.css">
+       
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <nav class="navbar navbar-static-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+            <h3 class="brandText">Wiseinfo LTD</h3>
+            </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><h3 class="userDropdown">Bride</h3><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Groom</a></li>
+                        <li><a href="#">Both</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><h3 class="userDropdown">All Photo</h3><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Unseen</a></li>
+                        <li><a href="#">Unrated</a></li>
+                        <li><a href="#">1 Star</a></li>
+                        <li><a href="#">2 Star</a></li>
+                        <li><a href="#">3 Star</a></li>
+                        <li><a href="#">4 Star</a></li>
+                        <li><a href="#">5 Star</a></li>
+                        
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><h3 class="userDropdown">Original Order</h3><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Winning Order</a></li>
+                        
+                    </ul>
+                </li>
+                <li><a href="#"><h3 class="userDropdown">Send</h3></a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><h3 class="userDropdown"><span class="glyphicon glyphicon-th" aria-hidden="true"></h3></a>
+                </li>
+                <li><a href="#"><h3 class="userDropdown"><span class="glyphicon glyphicon-film" aria-hidden="true"></h3></a>
+                </li>
+                <li><a href="#"><h3 class="userDropdown"><span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></h3></a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+
+        <section>
+            
+        <div class="row">
+          <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+              <img src="img/wedding-020.jpg" alt="...">
+              <div class="caption">
+              <center><h3>Wedding3.jpg</h3></center>
+                <center><h3><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a></h3></center>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+              <img src="img/wedding-020.jpg" alt="...">
+              <div class="caption">
+              <center><h3>Wedding2.jpg</h3></center>
+                <center><h3><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a></h3></center>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+              <img src="img/wedding-020.jpg" alt="...">
+              <div class="caption">
+                <center><h3>Wedding1.jpg</h3></center>
+                <center><h3><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a><a href="#"><span class="glyphicon glyphicon-star-empty"></span></a></h3></center>
+              </div>
+            </div>
+          </div>
+        </div>
+        </section>
+
+    </body>
+     <link rel="stylesheet" type="text/javascript" href="bootstrap/dist/js/bootstrap.js">
+        <link rel="stylesheet" type="text/javascript" href="bootstrap/dist/js/npm.js">
+</html>
